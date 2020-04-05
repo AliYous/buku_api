3e373712c1e77fc8a3df7c2503491a535e2c4f80
ali <aliyoussef34@hotmail.fr>
2020-04-05 18:37:02 +0200

    current_user helper and book controller done

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,9 @@ class ApplicationController < ActionController::API
 	rescue_from JWTSessions::Errors::Unauthorized, with: :not_authorized #Exception handling for unauthorized requests
   
   private
+  def current_user
+    @current_user ||= User.find(payload['user_id'])
+  end
   
   def not_authorized
     render json: { error: 'Not authorized' }, status: :unauthorized
