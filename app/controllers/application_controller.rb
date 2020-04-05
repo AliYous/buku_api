820f3a6e4c1a9eb7b42baff6c69d45936ff91395
ali <aliyoussef34@hotmail.fr>
2020-04-05 17:45:19 +0200

    jwt edit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::API
+	#Provides authorization actions which will secure the endpoints
+	include JWTSessions::RailsAuthorization
+	rescue_from JWTSessions::Errors::Unauthorized, with: :not_authorized #Exception handling for unauthorized requests
+  
+  private
+  
+  def not_authorized
+    render json: { error: 'Not authorized' }, status: :unauthorized
+  end
 end
