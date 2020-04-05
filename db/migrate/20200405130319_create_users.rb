ea4230fc9909ae2807ff0d7953fc1facb5027b72
ali <aliyoussef34@hotmail.fr>
2020-04-05 15:07:39 +0200

    added has secure pwd in user model

--- db/migrate/20200405130319_create_users.rb
+++ db/migrate/20200405130319_create_users.rb
@@ -1,8 +1,8 @@
 class CreateUsers < ActiveRecord::Migration[6.0]
   def change
     create_table :users do |t|
-      t.string :email
-      t.string :pasword_digest
+      t.string :email, null: false
+      t.string :pasword_digest, null: false
       t.string :first_name
 
       t.timestamps
