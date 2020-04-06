2e2883a49be84c43cb8d6ed0fb45a9f930a00392
ali <aliyoussef34@hotmail.fr>
2020-04-06 18:10:59 +0200

    API is functionnal, need to get authentication to work from the front end

--- app/controllers/api/v1/books_controller.rb
+++ app/controllers/api/v1/books_controller.rb
@@ -1,51 +1,55 @@
-class BooksController < ApplicationController
-	before_action :authorize_access_request!
-	before_action :set_book, only: [:show, :update, :destroy] #set the correct user book before update/delete/show
+module Api
+	module V1
+			class BooksController < ApplicationController
+				before_action :authorize_access_request!
+				before_action :set_book, only: [:show, :update, :destroy] #set the correct user book before update/delete/show
 
-	 # GET /books
-	 def index
-    @books = current_user.books
+				# GET /books
+				def index
+					@books = current_user.books
 
-    render json: @books
+					render json: @books
+				end
+				
+				# GET /books/1 not necessary for now
+				def show
+					render json: @book
+				end
+				
+				# POST /books
+				def create
+				@books = current_user.books.build(book_params)
+
+					if @book.save
+						render json: @book, status: :created, location: @book
+					else
+						render json: @book.errors, status: :unprocessable_entity
+					end
+				end
+
+				# PATCH/PUT /books/1
+				def update
+					if @book.update(book_params)
+						render json: @book
+					else
+						render json: @book.errors, status: :unprocessable_entity
+					end
+				end
+
+				# DELETE /books/1
+				def destroy
+					@book.destroy
+				end
+
+				private
+
+				def set_book
+					@book = current_user.books.find(params[:id])
+				end
+				
+				def book_params
+					params.require(:book).permit(:title, :author, :comment, :purchased, :status)
+				end
+			end
 	end
-	
-	# GET /books/1 not necessary for now
-  def show
-    render json: @book
-	end
-	
-	# POST /books
-	def create
-	@books = current_user.books.build(book_params)
-
-		if @book.save
-			render json: @book, status: :created, location: @book
-		else
-			render json: @book.errors, status: :unprocessable_entity
-		end
-	end
-
-	# PATCH/PUT /books/1
-	def update
-		if @book.update(book_params)
-			render json: @book
-		else
-			render json: @book.errors, status: :unprocessable_entity
-		end
-	end
-
-	# DELETE /books/1
-	def destroy
-		@book.destroy
-	end
-
-	private
-
-	def set_book
-		@book = current_user.books.find(params[:id])
-	end
-	
-	def book_params
-    params.require(:book).permit(:title, :author, :comment, :purchased, :status)
-  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      post 'refresh', controller: :refresh, action: :create
-      post 'signin', controller: :signin, action: :create
-      post 'signup', controller: :signup, action: :create
-      delete 'signin', controller: :signin, action: :destroy
       resources :books
     end
   end
+
+  post 'refresh', controller: :refresh, action: :create
+  post 'signin', controller: :signin, action: :create
+  post 'signup', controller: :signup, action: :create
+  delete 'signin', controller: :signin, action: :destroy
 
 end
